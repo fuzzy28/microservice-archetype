d7145a13b9e2cc44d97a693e9cc11bd53ecb366d
Joel F. Ruelos Jr. <ruelosjoel@gmail.com>
2016-05-14 13:50:54 +0800

    removed unused dependency

--- src/main/resources/archetype-resources/src/test/java/AbstractTest.java
+++ src/main/resources/archetype-resources/src/test/java/AbstractTest.java
@@ -7,9 +7,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.hazelcast.config.SecurityConfig;
-
 /**
  * Abstract Test which initialize the Spring boot application
  * 
