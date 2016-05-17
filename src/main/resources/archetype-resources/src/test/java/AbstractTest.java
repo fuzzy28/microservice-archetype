7da3fd20e31ac0656ed09dc57ae8c890a8ece1b4
Joel F. Ruelos Jr <joel.ruelosjr@infor.com>
2016-05-17 15:17:32 +0800

    Add web app config annotation

--- src/main/resources/archetype-resources/src/test/java/AbstractTest.java
+++ src/main/resources/archetype-resources/src/test/java/AbstractTest.java
@@ -7,15 +7,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
 /**
  * Abstract Test which initialize the Spring boot application
  * 
  * @author Joel F. Ruelos Jr.
  * @since 1.0
  */
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = { Application.class, SecurityConfig.class })
+@WebAppConfiguration
 public abstract class AbstractTest {
 
     @Test
