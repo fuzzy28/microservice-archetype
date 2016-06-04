650700e22375c030ef68f74d739968aca394b3e2
Joel F. Ruelos Jr. <ruelosjoel@gmail.com>
2016-06-04 14:53:59 +0800

    Remove useless test and enhance with dbunit.

--- src/main/resources/archetype-resources/src/test/java/service/__domainName__ServiceTest.java
+++ src/main/resources/archetype-resources/src/test/java/service/__domainName__ServiceTest.java
@@ -3,22 +3,18 @@
 #set( $symbol_escape = '\' )
 package ${package}.service;
 
-import javax.transaction.Transactional;
 import ${package}.AbstractIntegrationTest;
 import ${package}.domain.${domainName};
 import ${package}.service.${domainName}Service;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.ExpectedDatabase;
 import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
-
 import static org.junit.Assert.*;
 
 #set( $propertyIdentifier = "${propertyId.substring(0,1).toUpperCase()}${propertyId.substring(1)}")
 
-@Transactional
 @DatabaseSetup("initializeEmpty.xml")
 public class ${domainName}ServiceTest extends AbstractIntegrationTest {
 
@@ -28,7 +24,7 @@ public class ${domainName}ServiceTest extends AbstractIntegrationTest {
     @Test
     @ExpectedDatabase(
 	    assertionMode = DatabaseAssertionMode.NON_STRICT,
-	    value = "successfullyPersisted.xml")
+	    value = "persistingWithoutId_expected.xml")
     public void whenPersistingWithoutIdThenShouldPersist() {
 	// assert saving
 	${domainName} ${domainName.toLowerCase()} = new ${domainName}();
@@ -80,35 +76,13 @@ public class ${domainName}ServiceTest extends AbstractIntegrationTest {
     }
 
     @Test
-    public void whenSuccessfullyPersistedThenShouldRetrieve() {
+    @DatabaseSetup(value = "updateWithValidId.xml")
+    @ExpectedDatabase(
+	    assertionMode = DatabaseAssertionMode.NON_STRICT,
+	    value = "updateWithValidId_expected.xml")
+    public void whenUpdateWithValidIdThenShouldSucceed() {
 	// assert saving
 	${domainName} ${domainName.toLowerCase()} = new ${domainName}();
-	 #foreach($prop in $propertyList.split(","))
-		#set( $index = ${prop.indexOf("=")} )
-	    	#set( $name = ${prop.substring(0, $index)} )
-	    	#set( $capitalizeProp = "${name.substring(0,1).toUpperCase()}${name.substring(1)}")
-		#set( $index = $index + 1 )
-	    	#set( $type = ${prop.substring($index)} )
-
-	    	#if (!$name.toLowerCase().equals($propertyId.toLowerCase()))
-		    	#if ($type.toLowerCase().equals("string"))
-		    	    ${domainName.toLowerCase()}.set$capitalizeProp("Test");
-		    	#elseif ($type.toLowerCase().equals("boolean"))
-		    	    ${domainName.toLowerCase()}.set$capitalizeProp(true);
-			#elseif ($type.toLowerCase().equals("long"))
-		    	    ${domainName.toLowerCase()}.set$capitalizeProp(1L);
-			#elseif ($type.toLowerCase().equals("int"))
-			    ${domainName.toLowerCase()}.set$capitalizeProp(1);
-		    	#end
-	    	#end
-
-
-	 #end
-	 
-	${domainName} ${domainName.toLowerCase()}Persisted = ${domainName.toLowerCase()}Service.save(${domainName.toLowerCase()});
-	assertNotNull("failure - ${domainName.toLowerCase()} was not persisted", ${domainName.toLowerCase()}Persisted);
-
-	${domainName} ${domainName.toLowerCase()}2 = new ${domainName}();
 	 #foreach($prop in $propertyList.split(","))
 		#set( $index = ${prop.indexOf("=")} )
 	    	#set( $name = ${prop.substring(0, $index)} )
@@ -116,95 +90,17 @@ public class ${domainName}ServiceTest extends AbstractIntegrationTest {
 		#set( $index = $index + 1 )
 	    	#set( $type = ${prop.substring($index)} )
 	    	#if ($type.toLowerCase().equals("string"))
-	    	    ${domainName.toLowerCase()}2.set$capitalizeProp("Test");
+	    	    ${domainName.toLowerCase()}.set$capitalizeProp("Test Updated");
 	    	#elseif ($type.toLowerCase().equals("boolean"))
-	    	    ${domainName.toLowerCase()}2.set$capitalizeProp(true);
+	    	    ${domainName.toLowerCase()}.set$capitalizeProp(false);
 		#elseif ($type.toLowerCase().equals("long"))
-	    	    ${domainName.toLowerCase()}2.set$capitalizeProp(1L);
+	    	    ${domainName.toLowerCase()}.set$capitalizeProp(1L);
 		#elseif ($type.toLowerCase().equals("int"))
-		    ${domainName.toLowerCase()}2.set$capitalizeProp(1);
-	    	#end
+		    ${domainName.toLowerCase()}.set$capitalizeProp(1);
+	    	#end		    	
 	 #end
-	${domainName.toLowerCase()}2.set$propertyIdentifier(null);
-	assertNotNull("failure - ${domainName.toLowerCase()} was not persisted",
-		${domainName.toLowerCase()}Service.save(${domainName.toLowerCase()}2));
-
-	// assert find all
-	int total = ${domainName.toLowerCase()}Service.findAll().size();
-	assertEquals("failure - total size does not match", 2, total);
-
-	// assert find single
-	${domainName} actual = ${domainName.toLowerCase()}Service.findOne(${domainName.toLowerCase()}Persisted.get$propertyIdentifier());
-
-	 #foreach($prop in $propertyList.split(","))
-		#set( $index = ${prop.indexOf("=")} )
-	    	#set( $name = ${prop.substring(0, $index)} )
-	    	#set( $capitalizeProp = "${name.substring(0,1).toUpperCase()}${name.substring(1)}")
-		#set( $index = $index + 1 )
-	    	#set( $type = ${prop.substring($index)} )
-	    	assertEquals("failure - $name does not match", ${domainName.toLowerCase()}.get$capitalizeProp(),
-			actual.get$capitalizeProp());
-	 #end
-    }
-
-    @Test
-    public void whenUpdateWithValidIdThenShouldSucceed() {
-	// assert saving
-	${domainName} ${domainName.toLowerCase()} = new ${domainName}();
-	
-	 #foreach($prop in $propertyList.split(","))
-		#set( $index = ${prop.indexOf("=")} )
-	    	#set( $name = ${prop.substring(0, $index)} )
-	    	#set( $capitalizeProp = "${name.substring(0,1).toUpperCase()}${name.substring(1)}")
-		#set( $index = $index + 1 )
-	    	#set( $type = ${prop.substring($index)} )
-	    	#if (!$name.toLowerCase().equals($propertyId.toLowerCase()))
-		    	#if ($type.toLowerCase().equals("string"))
-		    	    ${domainName.toLowerCase()}.set$capitalizeProp("Test");
-		    	#elseif ($type.toLowerCase().equals("boolean"))
-		    	    ${domainName.toLowerCase()}.set$capitalizeProp(true);
-			#elseif ($type.toLowerCase().equals("long"))
-		    	    ${domainName.toLowerCase()}.set$capitalizeProp(1L);
-			#elseif ($type.toLowerCase().equals("int"))
-			    ${domainName.toLowerCase()}.set$capitalizeProp(1);
-		    	#end
-	    	#end
-	 #end
-	 
-	${domainName} ${domainName.toLowerCase()}Persisted = ${domainName.toLowerCase()}Service.save(${domainName.toLowerCase()});
-	assertNotNull("failure - ${domainName.toLowerCase()} was not persisted", ${domainName.toLowerCase()}Persisted);
-	
-	 #foreach($prop in $propertyList.split(","))
-		#set( $index = ${prop.indexOf("=")} )
-	    	#set( $name = ${prop.substring(0, $index)} )
-	    	#set( $capitalizeProp = "${name.substring(0,1).toUpperCase()}${name.substring(1)}")
-		#set( $index = $index + 1 )
-	    	#set( $type = ${prop.substring($index)} )
-	    	#if (!$name.toLowerCase().equals($propertyId.toLowerCase()))
-		    	#if ($type.toLowerCase().equals("string"))
-		    	    ${domainName.toLowerCase()}Persisted.set$capitalizeProp("Test Updated");
-		    	#elseif ($type.toLowerCase().equals("boolean"))
-		    	    ${domainName.toLowerCase()}Persisted.set$capitalizeProp(false);
-			#elseif ($type.toLowerCase().equals("long"))
-		    	    ${domainName.toLowerCase()}Persisted.set$capitalizeProp(1L);
-			#elseif ($type.toLowerCase().equals("int"))
-			    ${domainName.toLowerCase()}Persisted.set$capitalizeProp(1);
-		    	#end
-	    	#end
-	 #end
-	 
-	${domainName} modified;
 	assertNotNull("failure - ${domainName.toLowerCase()} was not updated",
-		modified = ${domainName.toLowerCase()}Service.update(${domainName.toLowerCase()}Persisted));
-	 #foreach($prop in $propertyList.split(","))
-		#set( $index = ${prop.indexOf("=")} )
-	    	#set( $name = ${prop.substring(0, $index)} )
-	    	#set( $capitalizeProp = "${name.substring(0,1).toUpperCase()}${name.substring(1)}")
-		#set( $index = $index + 1 )
-	    	#set( $type = ${prop.substring($index)} )
-	    	assertEquals("failure - $name does not match", ${domainName.toLowerCase()}Persisted.get$capitalizeProp(),
-	    		modified.get$capitalizeProp());
-	 #end
+		${domainName.toLowerCase()}Service.update(${domainName.toLowerCase()}));
     }
 
     @Test
